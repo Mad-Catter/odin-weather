0d044ce82fc37bb20427929be4b8792c20cafffb
Foley <teriomon5@aol.com>
2026-03-13 19:00:36 -0400

    project finished

--- src/changeDisplay.js
+++ src/changeDisplay.js
@@ -1,3 +1,13 @@
+import fog from './backgrounds/fog.gif';
+import sun from './backgrounds/sun.gif';
+import moon from './backgrounds/moon.gif';
+import weather from './backgrounds/weather.gif';
+import rain from './backgrounds/rain.gif';
+import snow from './backgrounds/snow.gif';
+import thunder from './backgrounds/thunder.gif';
+import wind from './backgrounds/wind.gif';
+import { giphyKey } from './key.js';
+
 const location = document.querySelector('#location');
 const icon = document.querySelector('#icon');
 const currentTemp = document.querySelector('.current');
@@ -5,16 +15,68 @@ const tempMax = document.querySelector('.max');
 const tempMin = document.querySelector('.min');
 const desc = document.querySelector('.desc');
 
-function changeDisplay(data) {
-  // Need to change the first letter to be capped
-  location.textContent = data.location;
-  // icon.src = ????
+async function changeDisplay(data) {
+  const svg = await import(`./weather-icons/${data.icon}.svg`);
+  icon.src = svg.default;
+  icon.alt = data.icon;
+  // The regex capitalizes the start of each word so something like: burkina faso becomes Burkina Faso
+  location.textContent = data.location.replace(/(^|\s)\S/g, (match) => match.toUpperCase());
+
   currentTemp.textContent = data.temp;
   tempMax.textContent = data.tempMax;
   tempMin.textContent = data.tempMin;
   desc.textContent = data.desc;
+  getBackground(data.icon);
+}
+async function getBackground(icon) {
+  // This function takes the icon given from data and then searches Giphy for a related background to use.
+  //   If there is an error, a preselected gif will be used as a background instead
+  let search;
+  let image;
+  const body = document.querySelector('body');
+  if (icon.includes('snow') || icon.includes('hail') || icon.includes('sleet')) {
+    search = 'snow';
+  } else if (icon.includes('rain') || icon.includes('showers')) {
+    search = 'rain';
+  } else if (icon.includes('thunder')) {
+    search = 'thunder';
+  } else if (icon.includes('cloud')) {
+    search = 'cloud';
+  } else if (icon.includes('wind')) {
+    search = 'wind';
+  } else if (icon.includes('fog')) {
+    search = 'fog';
+  } else if (icon.includes('day')) {
+    search = 'sun';
+  } else if (icon.includes('night')) {
+    search = 'moon';
+  } else {
+    search = 'weather';
+  }
+  try {
+    const url = `https://api.giphy.com/v1/gifs/translate?api_key=${giphyKey}&s=${search}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response);
+    }
+    const responseJSON = await response.json();
+    image = responseJSON.data.images.original.url;
+  } catch (error) {
+    console.log(error);
+    const backgrounds = {
+      fog,
+      sun,
+      moon,
+      weather,
+      rain,
+      snow,
+      thunder,
+      wind,
+    };
 
-  // GIPHY BACKGROUND CHANGE HERE
+    image = backgrounds[search];
+  }
+  body.style.backgroundImage = `url("${image}")`;
 }
 
 export default changeDisplay;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-import key from './key.js';
+import { weatherKey } from './key.js';
 import changeDisplay from './changeDisplay.js';
 import './style.css';
+
+const dialog = document.querySelector('#error');
+
 async function getWeather(location) {
-  async function getWeather(location) {
-    const link = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}${key}`;
+  // This function is wrapped in another function so that an error handler can be attached to it once, instead of needing to have .catch attached each call.
+  async function catchlessGetWeather(location) {
+    const link = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}${weatherKey}`;
     let response;
     try {
+      const now = new Date();
       response = await fetch(link);
+      console.log(`Visual Crossing request took: ${(new Date() - now) / 1000} seconds`);
     } catch (error) {
       console.log(error);
     }
     if (!response.ok) {
       const text = await response.text();
       console.log(text);
-      throw new Error(`Error: ${response.status}.  Message: ${text}`);
+      throw new Error(text);
     }
     const json = await response.json();
-    console.log(json);
     const data = {
       location: json.address,
       icon: json.currentConditions.icon,
@@ -25,29 +30,31 @@ async function getWeather(location) {
       tempMin: json.days[0].tempmin,
       desc: json.description,
     };
-    console.log(data);
     return data;
   }
-  return getWeather(location).catch((error) => {
-    console.log(error);
+  return catchlessGetWeather(location).catch((error) => {
+    dialog.textContent = error.message;
+    dialog.show();
+    return 'stop';
   });
 }
-const defaultLocation = 'belchertown';
 
-// getWeather(defaultLocation);
-getWeather(defaultLocation).catch((error) => {
-  console.log('weee');
-  console.log(error);
-});
 const button = document.querySelector('button');
 const search = document.querySelector('#search');
-button.addEventListener('click', async (e) => {
+button.addEventListener('click', async () => {
   let data = await getWeather(search.value);
-  changeDisplay(data);
+  if (data !== 'stop') await changeDisplay(data);
 });
-// Function to take a location and get data from the api.
-// Need to have an error handling for both if the user inputs nonsense or if the api is somehow broken.
-// After the json is retrieved.  Need a function that takes the Current temp, max and min temps of the day(day 0), the short description, current weather, and icon
-// This info is handed off into a new module for displaying the info.
-// The display module will display the current temps in bold white with a transparent background for visibility.  The entire page will have a background for these weathers:
-// Clear, rain, cloudy, thunder, snow, windy???
+search.addEventListener('keyup', async (e) => {
+  dialog.close();
+  if (e.key === 'Enter') {
+    let data = await getWeather(search.value);
+    if (data !== 'stop') await changeDisplay(data);
+  }
+});
+document.querySelector('body').addEventListener('click', () => {
+  dialog.close();
+});
+
+const firstRun = await getWeather('ouagadougou');
+changeDisplay(firstRun);
